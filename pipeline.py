621e9ea9efa3e2b4dd17ac61d1013d8d28a11cf2
arnavmarda <arnavmarda@gmail.com>
2023-07-03 05:42:13 +0530

    Debug statements for testing

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,11 @@ from create_tweet import handle_articles_list
 from post_tweet import tweet
 
 def scrape_and_tweet(topic: str, title: str, num_articles: int) -> None:
+    print("Scraping...")
     articles = scrape_articles(topic, num_articles)
+    print("Building list of articles...")
     articles = build_list_of_articles(articles)
+    print("Creating tweet...")
     to_post = handle_articles_list(articles, title)
+    print("Tweeting...")
     tweet(to_post)
